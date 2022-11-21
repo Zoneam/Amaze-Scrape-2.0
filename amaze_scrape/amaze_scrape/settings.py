f133ad597d3a37cb6ec8ad1825f20ce5beec0876
Zoneam <zoneam@gmail.com>
2022-11-21 01:00:09 -0800

    adding to heroku

--- amaze_scrape/amaze_scrape/settings.py
+++ amaze_scrape/amaze_scrape/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_URL = 'static/'
+STATIC_URL = 'main_app/static/'
 LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = '/search/'
 LOGOUT_REDIRECT_URL = '/login/'
