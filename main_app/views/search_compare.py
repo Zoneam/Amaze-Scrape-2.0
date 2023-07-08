3ba439f1ad95cbfb041bca8ef691b0167567db0c
Zoneam <zoneam@gmail.com>
2023-07-08 15:28:23 -0700

    logging errors

--- main_app/views/search_compare.py
+++ main_app/views/search_compare.py
@@ -30,19 +30,7 @@ HEADERSWM = ({
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'accept-encoding': 'gzip, deflate, br',
     'accept-language': 'en-US,en;q=0.9',
-    'method': 'GET',
-    'authority': 'www.walmart.com',
-    'sheme': 'https',
-    'cache-control': 'max-age=0',
     'referer': 'https://www.walmart.com/',
-    'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"macOS"',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'same-origin',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
     })
 
 def interceptor(request):
