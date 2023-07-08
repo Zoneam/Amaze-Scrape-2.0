80db1252678aecc43dde65bc8711ea32fd08d976
Zoneam <zoneam@gmail.com>
2023-07-08 02:33:41 -0700

    logging errors

--- main_app/views/search_compare.py
+++ main_app/views/search_compare.py
@@ -25,7 +25,7 @@ HEADERS = ({
     })
 
 HEADERSWM = ({ 
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
     'upgrade-insecure-requests': '1',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'accept-encoding': 'gzip, deflate, br',
